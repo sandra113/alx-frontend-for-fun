3c47676c226df75cd4da62f1729527482888e18b
sandra113 <sandramabonga303@gmail.com>
2024-03-24 13:18:45 -0700

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 """
 This is a script to convert a Markdown file to HTML.
-
-Usage:
-    ./markdown2html.py [input_file] [output_file]
-
-Arguments:
-    input_file: the name of the Markdown file to be converted
-    output_file: the name of the output HTML file
-
-Example:
-    ./markdown2html.py README.md README.html
 """
 
 import argparse
@@ -22,7 +12,6 @@ def convert_md_to_html(input_file, output_file):
     '''
     Converts markdown file to HTML file
     '''
-    # Read the contents of the input file
     with open(input_file, encoding='utf-8') as f:
         md_content = f.readlines()
 
@@ -31,9 +20,9 @@ def convert_md_to_html(input_file, output_file):
         # Check if the line is a heading
         match = re.match(r'(#){1,6} (.*)', line)
         if match:
-            # Get the level of the heading
+            # find the level of the heading
             h_level = len(match.group(1))
-            # Get the content of the heading
+            # find the content of the heading
             h_content = match.group(2)
             # Append the HTML equivalent of the heading
             html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
